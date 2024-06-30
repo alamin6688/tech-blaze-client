8213cec115babb956f1414468e1ff17c52c5ba69
alamin6688 <alamin.kzs06@gmail.com>
2024-07-01 00:29:26 +0600

    Navbar modified

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,44 +1,82 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [theme, setTheme] = useState("light");
 
-    const [theme, setTheme] = useState('light');
+  const navLinks = (
+    <>
+      <NavLink to="/" className="font-bold px-2">
+        Home
+      </NavLink>
 
-    useEffect(()=>{
-        localStorage.setItem('theme',theme)
-        const localTheme =localStorage.getItem('theme')
-        document.querySelector('html').setAttribute('data-theme', localTheme)
-    },[theme])
+      <NavLink to="/blogs" className="font-bold px-2">
+        Blogs
+      </NavLink>
 
-    const handleToggle = (e) => {
-        if(e.target.checked){
-            setTheme('synthwave')
-        }else{
-            setTheme('light')
-        }
+      <NavLink to="/bookmarks" className="font-bold px-2">
+        Bookmarks
+      </NavLink>
+    </>
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    const localTheme = localStorage.getItem("theme");
+    document.querySelector("html").setAttribute("data-theme", localTheme);
+  }, [theme]);
+
+  const handleToggle = (e) => {
+    if (e.target.checked) {
+      setTheme("synthwave");
+    } else {
+      setTheme("light");
     }
+  };
 
   return (
-    <div className="navbar bg-base-100 shadow-lg px-4 fixed z-10">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-secondary gap-0 text-2xl">
-            Tech<span className="text-primary">Blaze</span>
+    <div className="navbar bg-base-100 shadow-xl px-4 fixed z-10">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden px-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 shadow"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-secondary pl-1 gap-0 text-2xl">
+          Tech<span className="text-primary">Blaze</span>
         </a>
       </div>
-      <div className="flex-none">
-        <ul className="menu gap-4 menu-horizontal px-1">
-          <Link to='/' className="font-bold text-primary">
-            Home
-          </Link>
-          <Link to='/blogs' className="font-bold">
-            Blogs
-          </Link>
-          <Link to='/bookmarks' className="font-bold">
-            Bookmarks
-          </Link>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+      </div>
+      <div className="navbar-end gap-4">
+        <div>
           <label className="grid cursor-pointer place-items-center">
-            <input onChange={handleToggle}
+            <input
+              onChange={handleToggle}
               type="checkbox"
               className="toggle theme-controller bg-base-content col-span-2 col-start-1 row-start-1"
             />
@@ -72,7 +110,8 @@ const Navbar = () => {
               <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
             </svg>
           </label>
-        </ul>
+        </div>
+        <a className="btn btn-primary">Button</a>
       </div>
     </div>
   );
